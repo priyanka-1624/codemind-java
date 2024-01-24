a799c6eca7f25fbdd21ad45f5a0a503d9601d7e8
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-01-24 21:59:11 +0530

    import java.util.Scanner; public class AvgWeight {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int r;             r=a%100;             System.out.printf("%02d",r);
        }
    }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AvgWeight
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int r;
+            r=a%100;
+            System.out.printf("%02d",r);
+           
+    }
+}
