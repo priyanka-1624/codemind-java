640e3a599c7439da05c65c58ce6917f489fa96c8
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-01-25 11:03:45 +0530

    import java.util.Scanner; public class Salary {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         System.out.println((5*x+y*10)/z);
        }
    }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Salary
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        System.out.println((5*x+y*10)/z);
+        
+    }
+}
