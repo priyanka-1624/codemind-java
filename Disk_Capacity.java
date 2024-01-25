12555c6abdb73badfb0e9a4ca476639043f2a791
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-01-25 11:45:29 +0530

    import java.util.Scanner; public class DiskCapacity {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         int s=sc.nextInt();         int b=sc.nextInt();
            System.out.println(2*t*s*b*512);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DiskCapacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int s=sc.nextInt();
+        int b=sc.nextInt();
+        
+        System.out.println(2*t*s*b*512);
+    }
+}
