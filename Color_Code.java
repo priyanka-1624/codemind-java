81680d7a937467a3592fe1a45712ab377011f32c
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-01-30 13:42:00 +0530

    import java.util.Scanner; public class Colors {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         switch(ch)         {             case 'V' :System.out.println("Violet");break;             case 'I':System.out.println("Indigo");break;             case 'B':System.out.println("Blue");break;             case 'G':System.out.println("Green");break;             case 'Y':System.out.println("Yellow");break;             case 'O':System.out.println("Orange");break;             case 'R':System.out.println("Red");break;             case 'v' :System.out.println("Violet");break;             case 'i':System.out.println("Indigo");break;             case 'b':System.out.println("Blue");break;             case 'g':System.out.println("Green");break;             case 'y':System.out.println("Yellow");break;             case 'o':System.out.println("Orange");break;             case 'r':System.out.println("Red");break;             default:System.out.println(-1);         }     } }

---
+++ Color_Code.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Colors
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case 'V' :System.out.println("Violet");break;
+            case 'I':System.out.println("Indigo");break;
+            case 'B':System.out.println("Blue");break;
+            case 'G':System.out.println("Green");break;
+            case 'Y':System.out.println("Yellow");break;
+            case 'O':System.out.println("Orange");break;
+            case 'R':System.out.println("Red");break;
+            case 'v' :System.out.println("Violet");break;
+            case 'i':System.out.println("Indigo");break;
+            case 'b':System.out.println("Blue");break;
+            case 'g':System.out.println("Green");break;
+            case 'y':System.out.println("Yellow");break;
+            case 'o':System.out.println("Orange");break;
+            case 'r':System.out.println("Red");break;
+            default:System.out.println(-1);
+        }
+    }
+}
