ebe89db95cae552322b0193f24e2e35e428aa0d0
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-01-31 11:06:52 +0530

    import java.util.Scanner; public class CarCost {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char ch=sc.nextLine().charAt(0);         if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch==         'u')         {             System.out.println("Vowel");         }         else         {             System.out.println("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CarCost
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.nextLine().charAt(0);
+        if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch==
+        'u')
+        {
+            System.out.println("Vowel");
+        }
+        else
+        {
+            System.out.println("Consonant");
+        }
+    }
+}
