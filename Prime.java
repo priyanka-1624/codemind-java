848759140c9385114f2ef71cdf01ebc2fec7057b
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-02-01 11:24:46 +0530

    import java.util.Scanner; public class PrimeorNot {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         for(int i=1;i<=n;i++)         {             if(n%i==0)             {                 c=c+1;             }         }         if (c==2)         {             System.out.println("Prime");         }         else         {             System.out.println("Not Prime");         }     } }

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PrimeorNot
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                c=c+1;
+            }
+        }
+        if (c==2)
+        {
+            System.out.println("Prime");
+        }
+        else
+        {
+            System.out.println("Not Prime");
+        }
+    }
+}
