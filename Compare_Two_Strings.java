76a339cc8f0cca8b4e7f17db85c3bc882f778733
Padala Gowtamipadmapriyanka <150513616+priyanka-1624@users.noreply.github.com>
2024-02-17 10:58:02 +0530

    import java.util.Scanner; public class LuckyChar {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2=sc.nextLine();         if(s1.equals(s2))            System.out.println("Strings are Equal");         else            System.out.println("Strings are not Equal");     } }

---
+++ Compare_Two_Strings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LuckyChar
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        if(s1.equals(s2))
+           System.out.println("Strings are Equal");
+        else
+           System.out.println("Strings are not Equal");
+    }
+}
